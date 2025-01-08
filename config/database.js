7435d5341340dccd87af143d3fab2d3759463275
paulj <pauljorhahaha@gmail.com>
2025-01-09 00:45:00 +0800

    CHORE: update db config

--- config/database.js
+++ config/database.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`, {
+        const dbUri = `${process.env.MONGO_URI}/${process.env.DB_NAME}`;
+        const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+            connectTimeoutMS: 10000,
+        };
+        await mongoose.connect(dbUri, options);
         console.log(`Connected to database: ${process.env.DB_NAME}`);
     } catch (err) {
-        console.error("MongoDB connection error:", err);
+        console.error("MongoDB connection error:", err.message);
         process.exit(1);
     }
 };
