479204f649d91105c060e39cab3fa9b1aa38b649
paulj <pauljorhahaha@gmail.com>
2025-01-15 21:14:09 +0800

    CHORE: remove books

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { swaggerUi, swaggerSpec } = require("./swagger/config");
 const connectDB = require("./config/database");
-const booksRoutes = require("./routes/books.js");
-const usersRoutes = require("./routes/users.js");
 const teacherRoutes = require("./routes/teachers.js");
 const studentRoutes = require("./routes/students.js");
 
@@ -21,8 +19,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/", router);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/books", booksRoutes);
-app.use("/users", usersRoutes);
 app.use("/teacher", teacherRoutes);
 app.use("/student", studentRoutes);
 
